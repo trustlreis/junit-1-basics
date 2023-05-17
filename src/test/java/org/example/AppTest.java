4c397aa4d14ab6701af085e1142310683a4e212b
Luis Machado Reis <luis.reis@trustly.com>
2023-05-17 18:23:13 -0300

    JUnit Basics

---
+++ src/test/java/org/example/AppTest.java
@@ -0,0 +1,49 @@
+package org.example;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+
+    private static final String APP1_NAME = "App1";
+    private static final String APP2_NAME = "App2";
+
+    private App app;
+
+    @BeforeEach
+    void tearUp() {
+        System.out.println("tearUp");
+        app = new App(APP1_NAME);
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("tearDown");
+        app = null;
+    }
+
+    @Test
+    void testConstructor() {
+        Assertions.assertEquals(APP1_NAME, app.getName());
+    }
+
+    @Test
+    void testRunning() {
+        Assertions.assertDoesNotThrow(() -> {
+            app.run();
+        });
+
+        Assertions.assertTrue(app.isRunning());
+    }
+
+    @Test
+    void testRunningInvalidTest() {
+        Assertions.assertThrows(IllegalStateException.class, () -> {
+            app.shutdown();
+        });
+
+        Assertions.assertFalse(app.isRunning());
+    }
+}
